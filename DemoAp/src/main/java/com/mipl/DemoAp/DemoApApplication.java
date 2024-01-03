edf7ed2d84ab75d1ece80eeaa9efe951e9c23b82
AdamsArts <adambhai.mipl@gmail.com>
2024-01-03 16:43:42 +0530

    Commited

--- DemoAp/src/main/java/com/mipl/DemoAp/DemoApApplication.java
+++ DemoAp/src/main/java/com/mipl/DemoAp/DemoApApplication.java
@@ -9,5 +9,6 @@ public class DemoApApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApApplication.class, args);
 	}
+	
 
 }
--- DemoAp/src/main/java/com/mipl/DemoAp/Employee.java
+++ DemoAp/src/main/java/com/mipl/DemoAp/Employee.java
@@ -4,19 +4,18 @@ public class Employee {
 	private String name;
 	private int empNo;
 	private String role;
-	
+
 	public void print() {
 		// TODO Auto-generated method stub
-		System.out.println("Hii This is "+name+" Employee id is "+empNo+" And i am working in "+role+" role");
-		
+		System.out
+				.println("Hii This is " + name + " Employee id is " + empNo + " And i am working in " + role + " role");
 
 	}
-	
-	
-	
+
 	public Employee() {
 		// TODO Auto-generated constructor stub
 	}
+
 	public Employee(String name, int empNo, String role) {
 		super();
 //		System.out.println("This output from Constructor");
@@ -24,27 +23,30 @@ public class Employee {
 		this.empNo = empNo;
 		this.role = role;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 //		System.out.println("This output from setters methods");
 		this.name = name;
 	}
+
 	public int getEmpNo() {
 		return empNo;
 	}
+
 	public void setEmpNo(int empNo) {
 		this.empNo = empNo;
 	}
+
 	public String getRole() {
 		return role;
 	}
+
 	public void setRole(String role) {
 		this.role = role;
 	}
-	
-	
 
 }
