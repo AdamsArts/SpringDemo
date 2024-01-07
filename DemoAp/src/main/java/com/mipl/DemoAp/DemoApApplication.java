2ff9cf327cdbf4ceaba6ab6959956f6f1806f8b0
AdamsArts <adambhai.mipl@gmail.com>
2024-01-07 14:09:36 +0530

    commited

--- DemoAp/src/main/java/com/mipl/DemoAp/DemoApApplication.java
+++ DemoAp/src/main/java/com/mipl/DemoAp/DemoApApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApApplication.class, args);
+		SpringApplication.run(DemoApApplication.class, args);		
 	}
 	
 	
