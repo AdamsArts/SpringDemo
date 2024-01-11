76647c5ff45361cd80cc88866440569344943734
AdamsArts <adambhai.mipl@gmail.com>
2024-01-11 12:21:17 +0530

    New project

---
+++ Gadgets/src/main/java/com/mipl/project/Gadgets/GadgetsApplication.java
@@ -0,0 +1,13 @@
+package com.mipl.project.Gadgets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadgetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GadgetsApplication.class, args);
+	}
+
+}
---
+++ Gadgets/src/test/java/com/mipl/project/Gadgets/GadgetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mipl.project.Gadgets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadgetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
