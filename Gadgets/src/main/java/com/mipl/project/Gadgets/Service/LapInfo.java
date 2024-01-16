1467a0d344103276bdfb916840c227645a12544b
AdamsArts <adambhai.mipl@gmail.com>
2024-01-16 11:04:45 +0530

    updated

--- Gadgets/src/main/java/com/mipl/project/Gadgets/Service/LapInfo.java
+++ Gadgets/src/main/java/com/mipl/project/Gadgets/Service/LapInfo.java
@@ -8,30 +8,20 @@ import org.springframework.stereotype.Service;
 
 import com.mipl.project.Gadgets.Entity.Laptop;
 
-
-
 @Service
 public class LapInfo {
-	
-	public static List<Laptop> lapDetails=new ArrayList<>(
-			Arrays.asList(new Laptop(1,"Dell",45000,"Windows 10"),
-					new Laptop(2,"HP",48000,"Windows 11")));
+
+	public static List<Laptop> lapDetails = new ArrayList<>(
+			Arrays.asList(new Laptop(1, "Dell", 45000, "Windows 10"), new Laptop(2, "HP", 48000, "Windows 11")));
 
 	public static List<Laptop> lapCollection() {
-		
+
 		return lapDetails;
 	}
 
-	
-
 	public static String insertLap(Laptop laptop) {
 		lapDetails.add(laptop);
-System.out.println("inserted");
 		return "Record inserted";
 	}
-
-
-	
-	
 
 }
