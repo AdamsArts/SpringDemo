563b38651bac0c92ecac27c18b426aa26723e754
AdamsArts <adambhai.mipl@gmail.com>
2024-01-18 11:45:28 +0530

    updated

--- Employee/src/main/java/com/mipl/Employee/Controller/EmpController.java
+++ Employee/src/main/java/com/mipl/Employee/Controller/EmpController.java
@@ -3,6 +3,7 @@ package com.mipl.Employee.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,24 +17,33 @@ import com.mipl.Employee.Service.EmpService;
 @RestController
 @RequestMapping("/Employee")
 public class EmpController {
-	
-	
+
 	@GetMapping("/employeeList")
-	public List<Employee> employees(){
-		
+	public List<Employee> employees() {
+
 		return EmpService.listOfEmployees();
 	}
-	
+
 	@GetMapping("getEmployee/{id}")
-	public Employee findEmpById(@PathVariable("id") int eid) {
-		return EmpService.findById(eid);
+	public Employee findEmpById(@PathVariable("id") int id) {
+
+		return EmpService.findById(id);
 	}
-	
+
+	@DeleteMapping("deleteEmployee/{id}")
+	public String deleteEmpById(@PathVariable int id) {
+
+		return EmpService.deleteById(id);
+	}
+
+	@PostMapping("updateEmployee/{id}")
+	public String updateEmployeeById(@RequestBody Employee updatedEmp, @PathVariable int id) {
+		return EmpService.updateEmpById(updatedEmp, id);
+	}
+
 	@PostMapping("/addemp")
 	public String addEmployee(@RequestBody Employee employee) {
-//		return EmpService.insertEmpByStream(employee);
-		
-		
+
 		return EmpService.insertEmp(employee);
 	}
 
--- Employee/src/main/java/com/mipl/Employee/Service/EmpService.java
+++ Employee/src/main/java/com/mipl/Employee/Service/EmpService.java
@@ -12,8 +12,9 @@ import com.mipl.Employee.Entity.Employee;
 @Service
 public class EmpService {
 
-	public static List<Employee> employees = new ArrayList<>();
-	
+	public static List<Employee> employees = new ArrayList<>(
+			Arrays.asList(new Employee(1, "Adam", "Developer"), new Employee(2, "Madhu", "Developer")));
+
 //	Arrays.asList(new Employee(1, "Adam", "Developer"), new Employee(2, "Madhu", "Developer"))
 
 	public static List<Employee> listOfEmployees() {
@@ -39,9 +40,31 @@ public class EmpService {
 		return res;
 	}
 
-	public static String insertEmpByStream(Employee employee) {
-		// TODO Auto-generated method stub
-		return null;
+	public static String deleteById(int id) {
+
+		for (Employee employee : employees) {
+			if (employee.getEmp_Id() == id) {
+				employees.remove(employee);
+				return employee.getEmp_Name() + "'s Record deleted successfully";
+			}
+
+		}
+
+		return "Record not found";
+	}
+
+	public static String updateEmpById(Employee updatedEmp, int id) {
+		
+		for (Employee employee : employees) {
+			if (employee.getEmp_Id() == id) {
+				employee.setEmp_Id(updatedEmp.getEmp_Id());
+				employee.setEmp_Name(updatedEmp.getEmp_Name());
+				employee.setEmp_role(updatedEmp.getEmp_role());
+				return "Record updated";
+			}
+		}
+		
+		return "Oops Record not found!!!";
 	}
 
 }
