0abb92e0c9f65bc4e25be8d6cf36afac502bc1b2
AdamsArts <adambhai.mipl@gmail.com>
2024-01-19 21:29:33 +0530

    updated

--- PatientManagement/src/main/java/com/mipl/PatientManagement/Controller/PatientController.java
+++ PatientManagement/src/main/java/com/mipl/PatientManagement/Controller/PatientController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class PatientController {
 	public List<Patient> getPatients(){
 		return PatientService.getPatients();
 	}
-	@PostMapping("/updatePatient/{id}")
+	@	PutMapping("/updatePatient/{id}")
 	public String updatePatient(@PathVariable int id,@RequestBody Patient updatedData) {
 		return PatientService.updateData(id,updatedData);
 	}
