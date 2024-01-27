416f909b39b166aafe2d396365776db3d31ddb28
AdamsArts <adambhai.mipl@gmail.com>
2024-01-27 21:03:45 +0530

    save

--- Employee-Management/src/main/java/com/project/Management/Entity/Employee.java
+++ Employee-Management/src/main/java/com/project/Management/Entity/Employee.java
@@ -10,18 +10,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 
 @Entity
 @Table
 public class Employee implements Serializable{
-	
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column
 	private int id;
 	@Column
 	private String name;
